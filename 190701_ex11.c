0497c33add9608d4410c25869bf5c11171c24efc
박인성 (Bahk InSung) <insung.park123@gmail.com>
2019-07-01 14:05:30 +0900

    Create 190701_ex11.c

---
+++ 190701_ex11.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int a[3] = { 10, 20, 30 };
+	int *p = a;
+	for (int i = 0; i < 3; i++) {
+		printf("  배열 a[%d]의 값 : %d\n", i, a[i]);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		printf("포인터 p[%d]의 값 : %d\n", i, p[i]);
+	}
+	p[0] = 100;
+	p[1] = 200;
+	p[2] = 300;
+	printf("###########################\n");
+	for (int i = 0; i < 3; i++) {
+		printf("  배열 a[%d]의 값 : %d\n", i, a[i]);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		printf("포인터 p[%d]의 값 : %d\n", i, p[i]);
+	}
+
+	printf("a자체의 값 : %d\n", a);
+	printf("a[0]의 주소 : %d\n", &a[0]);
+	printf("a자체의 값이 가지는 주소의 실제 값 : %d\n", *a);
+	return 0;
+}
